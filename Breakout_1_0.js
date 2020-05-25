05178d81c16cca171c48d8f14947ae18469c28db
Ayoola David <ayooladavid@gmail.com>
2020-05-25 16:17:48 +0100

    cleanup breakout

--- Breakout_1_0.js
+++ Breakout_1_0.js
@@ -15,8 +15,7 @@ var paddleHeight = 10, paddleWidth = 75, paddleX = (canvas.width-paddleWidth)/2;
 var dPressed = false, aPressed = false;
 
 var brickRowCount = 4, brickColumnCount = 8, brickWidth = 50, brickHight = 20, brickPadding = 5, brickOffsetTop = 30, brickOffsetleft = 25;
-/*skal bruges til de bricks man skal ramme med bolden
-Der er defineret en variable som bestemmer antallet af rækker, antallet af bricks pr. række, højden og bredden af de bricks. Samtidig er der defineret en variable så de ikke rammer hinanden og en variable så de ikke bliver tegnet fra toppen og ved kanten, hvilket giver lidt space til kanten af canvas*/
+
 var bricks = []
 for(var c=0; c<brickColumnCount; c++){
   bricks[c] = [];
@@ -81,13 +80,7 @@ function collisionDetection(){
     }
   }
 }
-/*function for at bolden skal ændre retning ved impact med bricks. Her er der defineret en variable som dækker over x og y koordinaterne for vores bricks, b-variablen. Den if statement skal der være opgyldt 4 ting, før bolden ændre retning, 
-x positionen for bolden, skal være større end x positionen for bricks 
-x positionen for bolden, skal være mindre end x positionen for bricks plusset med brickWidth
-y positionen for bolden, skal være større end y positionen for bricks 
-y positionen for bolden, skal være mindre end y positionen for bricks plusset med brickHight
-der er plusset med 10 ved x og trukket 10 fra ved y, så det ligner den rammer ved kanten af bolden, istedet for i midten af bolden. 
-sidste del er for at kunne tegne scoren*/
+
 
 function drawScore(){
   ctx.font = "16 px Arial";
@@ -196,8 +189,7 @@ function draw() {
   drawHighScore();
   drawBricks();
   glitchdetectorx();
-  /*henter funktionen drawBall, drawPaddle & drawBricks
-  Få bolden til at "bounce" ved impact af siderne af canvas*/
+  
 
   if(x + ballx > canvas.width-ballRadius || x + ballx < ballRadius){
     ballx = -ballx;
@@ -220,9 +212,7 @@ function draw() {
           clearInterval(interval);
       }
   }
-  /* || er en form for eller - så hvis enten den ene eller den anden statement er true, ændres retningen af bolden. 
-  Der er trukket ballRadius fra og sat "< ballRadius" sådan så bolden ikke synker ind i rammen. Så ligner det, at bolden ændre kurs, når kanten af bolden rammer rammen */
-
+  
  if(dPressed) {
         paddleX += 7;
         if (paddleX + paddleWidth > canvas.width){
@@ -235,8 +225,7 @@ function draw() {
             paddleX = 0;
         }
     }
-  /*får paddlen til at bevæge sig til højre eller venstre, men Samtidig ikke udenfor canvas, er der nemlig taget højde for oppe ved den anden if statement. tallet 7 definerer hastigheden paddlen bevæger sig med, når der trykkes. Om det er en fejl, eller mangle på erfaring, kan diskuterres, men nås et vist niveau, vil bolden bevæge sig så hurtigt, at selve paddle ikke kan følge med*/
-
+  
   x += ballx;
   y += bally;
   
